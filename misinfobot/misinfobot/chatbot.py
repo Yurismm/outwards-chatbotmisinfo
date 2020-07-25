69b31217855af9b4ce989712039ac2eabf16fc3f
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2020-07-25 03:57:02 +0100

    Update chatbot.py
    6

--- misinfobot/misinfobot/chatbot.py
+++ misinfobot/misinfobot/chatbot.py
@@ -14,15 +14,23 @@ print("Tell me what Twitter profile you would like to analyse. Can be yours, or 
 time.sleep(1)
 screenname = input("Insert Screenname here:")
 
+wltkm = input("Thank you. I am now gathering information tweeted by @" + screenname + ". In the meantime, would you like to learn more about how Misinfo.Me works?")
+### Will continue this at a later time.
 
 ### reading json file + screenname
 
 response = requests.get("https://socsem.kmi.open.ac.uk/misinfo/api/credibility/users/?screen_name=" + screenname)
 response.raise_for_status()
 jsonResponse = response.json()
-print("Credibility value(s) (the closer to 1 it is, the more reliable it is:")
+print("Credibility value(s) (the closer to 1 it is, the more reliable it is, however, if it's in minus numbers that means the profile could be spreading misinfomation.")
 value = jsonResponse["credibility"]["value"]
 print(value)
+tweetcount = jsonResponse["itemReviewed"]["tweets_cnt"]
+print("@" + screenname + " has " + str(tweetcount) + " tweets in total.")
+sharedurlcount = jsonResponse["itemReviewed"]["shared_urls_cnt"]
+print("@" + screenname + " has " + str(sharedurlcount) + " urls shared in tweets in total.")
+
+
 ### Prints the value, we really need to make this number into something more user-friendly.
 
 
