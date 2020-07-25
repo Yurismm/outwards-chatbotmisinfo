a46721ff523a27f9be6e10a776c751c7ff3b9795
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2020-07-25 16:44:37 +0100

    f

--- misinfobot/misinfobot/chatbot.py
+++ misinfobot/misinfobot/chatbot.py
@@ -14,7 +14,13 @@ print("Tell me what Twitter profile you would like to analyse. Can be yours, or 
 time.sleep(1)
 screenname = input("Insert Screenname here:")
 
-wltkm = input("Thank you. I am now gathering information tweeted by @" + screenname + ". In the meantime, would you like to learn more about how Misinfo.Me works?")
+wltkm = input("Thank you. I am now gathering information tweeted by @" + screenname + ". In the meantime, would you like to learn more about how Misinfo.Me works? (yes/no)")
+
+if wltkm == "yes":
+    print("Misinfo.Me extracts the links that part of any tweet created or retweet by the profile under analysis. We then look for these URLs that have been analysed by fact-checkers and check their sources. Based on these assessments, we calculate a score to the Twitter profile that will be presented to you.")
+elif wltkm =="no":
+    print("Alright. That could be important information later, so if you want to find out more about the score, just ask ‘can I find out more about the score’ once your analysis is complete.")
+
 ### Will continue this at a later time.
 
 ### reading json file + screenname
@@ -28,7 +34,7 @@ print(value)
 tweetcount = jsonResponse["itemReviewed"]["tweets_cnt"]
 print("@" + screenname + " has " + str(tweetcount) + " tweets in total.")
 sharedurlcount = jsonResponse["itemReviewed"]["shared_urls_cnt"]
-print("@" + screenname + " has " + str(sharedurlcount) + " urls shared in tweets in total.")
+print("@" + screenname + " has " + str(sharedurlcount) + " urls have been shared in their tweets in total.")
 
 
 ### Prints the value, we really need to make this number into something more user-friendly.
