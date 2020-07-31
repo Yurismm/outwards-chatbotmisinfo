27fcc16d3e68dbaac23930a1e0141000ed368249
Azizah Cherie Blackwood <40805624+Yurismm@users.noreply.github.com>
2020-07-31 22:47:45 +0100

    extras

--- misinfobot/misinfobot/chatbot.py
+++ misinfobot/misinfobot/chatbot.py
@@ -8,6 +8,7 @@ print(response.status_code)
 
 time.sleep(5)
 
+
 print("Hello! I am Misinfo.Me.Bot and I am here to help you to analyse Twitter profiles for you to know who are those spreading misinformation in your network.")
 time.sleep(3)
 print("Tell me what Twitter profile you would like to analyse. Can be yours, or someone that you suspect that spreads a lot of misinformation, like Donald Trump, for example ;-) . Please, type the Twitter name after @, like @realDonaldTrump, for example.")
@@ -21,8 +22,6 @@ if wltkm == "yes":
     print("Misinfo.Me extracts the links that part of any tweet created or retweet by the profile under analysis. We then look for these URLs that have been analysed by fact-checkers and check their sources. Based on these assessments, we calculate a score to the Twitter profile that will be presented to you.")
 elif wltkm =="no":
     print("Alright. That could be important information later, so if you want to find out more about the score, just ask ‘can I find out more about the score’ once your analysis is complete.")
-
-### Will continue this at a later time.
 
 ### reading json file + screenname
 
@@ -51,10 +50,28 @@ print("@" + screenname + " has " + str(sharedurlcount) + " urls have been shared
 print("@" + screenname + " has " + str(fkcnt) + " urls that have been labled as fake links.")
 print("@" + screenname + " has " + str(verifcnt) + " urls that have been labled as verified links (This could be 0, because the profile isn't analysed fully yet.")
 print("@" + screenname + " has " + str(unkcnt) + " urls that from unknown places, or that are unknown to us.")
-print("This code was last updated on" + str(lastupdated))
+print("This result was last updated on: " + str(lastupdated))
+time.sleep(2)
+if value < 0:
+    print("This means that this profile could possibilty be associated with misinfomation")
+elif value > 0:
+    print("This means that this profile probably isn't associated with misinfomation")
 time.sleep(2)
 
-input("You can also send a link to a tweet to have it analysed for misinfomation. Would you like to do this? (y/n)")
+#at stands for anaylse tweet.
 
+at = input("You can also send a link to a tweet to have it analysed for misinfomation. Would you like to do this? (y/n)")
+if at == "y":
+    tweetid = input("Please insert the ID of the tweet that you would like to gain analyasis from:")
+    response = requests.get("https://socsem.kmi.open.ac.uk/misinfo/api/credibility/tweets/" + tweetid + "?wait=true")
+    response.raise_for_status()
+    jsonResponse = response.json()
+    tweetcred = jsonResponse["credibility"]["value"]
+    print("The credibility value is: " + str(tweetcred))
+    if tweetcred < 0:
+        print("This tweet could include sources that are known to have misinfomaiton.")
+    elif tweetcred > 0:
+        print("This tweet probably doesn't have any misinfomation.")
+elif at == "n":
+    print("That's alright. Thank you for using MisinfoMeBot.")
 
-### Prints the value, we really need to make this number into something more user-friendly.
